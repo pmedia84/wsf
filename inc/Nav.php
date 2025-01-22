8ef699bcb0319ada25d3fc06694774fceb879ac3
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2025-01-22 13:45:46 +0000

    nav-brand link

--- inc/Nav.php
+++ inc/Nav.php
@@ -1,5 +1,5 @@
  <nav class="navbar navbar-expand-md fixed-top bg-body py-3 border-bottom">
-    <div class="container"><a class="navbar-brand" href="#"><img class="brand-logo" src="assets/img/logo-2.svg" /></a><button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+    <div class="container"><a class="navbar-brand" href="https://www.windowsupportfund.uk"><img class="brand-logo" src="assets/img/logo-2.svg" /></a><button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div id="navcol-1" class="collapse navbar-collapse d-md-flex justify-content-md-end">
             <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link active" href="/">Home</a></li>
